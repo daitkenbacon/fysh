969bf8416528a1f07917131b4e21f871b3721943
daitkenbacon <104740593+daitkenbacon@users.noreply.github.com>
2023-01-27 21:15:10 -0600

    CatchForm submit button quickfix

--- fysh-app/src/components/catch-form/catch-form.component.jsx
+++ fysh-app/src/components/catch-form/catch-form.component.jsx
@@ -137,6 +137,7 @@ const CatchForm = (props) => {
                 </label>
                 <div className="relative mt-1 rounded-md shadow-sm">
                     <input
+                    required={true}
                     type="text"
                     name="description"
                     maxLength={35}
@@ -161,6 +162,7 @@ const CatchForm = (props) => {
                     id="size"
                     className="mb-2 block w-full rounded-md border-gray-300 pl-3 pr-3 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                     placeholder="15"
+                    required={true}
                     />
                 </div>
             </div>
@@ -188,7 +190,7 @@ const CatchForm = (props) => {
             }
             
             <div className="bg-gray-50 w-full px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                <button type="button" className="inline-flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">Submit</button>
+                <button type="submit" className="inline-flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">Submit</button>
                 <button onClick={() => setOpenModal(false)} type="button" className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
             </div>
         </form>
